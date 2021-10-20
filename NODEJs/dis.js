baeeebe85705b5d7588182af771a2d22edadbdde
ankitpb <ankitpb@example.com>
2021-10-21 00:05:31 +0530

    first commit

---
+++ NODEJs/dis.js
@@ -0,0 +1,13 @@
+    <script type="text/javascript">
+        function check()
+         {
+            if(navigator.userAgent.match(/iPad/i) 
+            {
+                userAgent = 'iOS';}
+            } 
+            else if (navigator.userAgent.match(/Android/i))
+            {
+                userAgent = 'Android';
+            }
+         }
+    </script>
